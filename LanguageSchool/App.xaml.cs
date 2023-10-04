44e7c158f02126fb92bb7f52e53e47a09e203c41
Kirill <lop.roi@bk.ru>
2023-10-04 08:36:33 +0300

    add database

--- LanguageSchool/App.xaml.cs
+++ LanguageSchool/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LanguageSchool.Components;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,7 @@ namespace LanguageSchool
     /// </summary>
     public partial class App : Application
     {
+          public static SCHOOLEntities db =
+             new SCHOOLEntities();
     }
 }
